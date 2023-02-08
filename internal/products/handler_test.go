0602828fc110ed8c0e4a548b5d1363bbcee77532
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-08 14:23:10 -0300

    func createproducts in handler_test

--- internal/products/handler_test.go
+++ internal/products/handler_test.go
@@ -1,2 +1,20 @@
 package products
 
+import "github.com/gin-gonic/gin"
+
+
+func CreateProductServer(rp Repository) *gin.Engine{
+	ser := NewService(rp)
+
+	server := gin.Default()
+
+	router := server.Group("/api/v1")
+	{
+		h:= NewHandler(ser)
+		group := router.Group("/products")
+		group.GET("", h.GetProducts)
+	}
+
+	return server
+}
+
