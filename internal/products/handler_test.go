c9a35af34b5dd6b490257e52bbf472af1f60df2a
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-08 14:28:08 -0300

    func new request created

--- internal/products/handler_test.go
+++ internal/products/handler_test.go
@@ -1,6 +1,12 @@
 package products
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 
 func CreateProductServer(rp Repository) *gin.Engine{
@@ -18,3 +24,13 @@ func CreateProductServer(rp Repository) *gin.Engine{
 	return server
 }
 
+func NewRequest(method, path, body string) (req *http.Request, res *httptest.ResponseRecorder) {
+	// request
+	req = httptest.NewRequest(method, path, strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/json")
+
+	// response
+	res = httptest.NewRecorder()
+
+	return
+}
